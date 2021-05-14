9788faff8d602bf5ceb4fbba86c0602343ee74ca
Brian Wilson <squarehippobills@gmail.com>
2021-05-14 12:06:15 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brian Wilson on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
